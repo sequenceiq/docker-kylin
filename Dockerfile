f7c12ac0222a2e1034774f1be351cdd5bae5abf7
shaofengshi <shaofengshi@apache.org>
2015-07-20 21:56:21 +0800

    Add APP_TIMELINE_SERVER component

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,11 @@
 FROM sequenceiq/ambari:1.7.0
 MAINTAINER SequenceIQ
 
-RUN curl -s http://xenia.sote.hu/ftp/mirrors/www.apache.org/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz | tar -xz -C /usr/local/
-RUN ln -s /usr/local/apache-maven-3.2.5/bin/mvn /usr/bin/mvn
-
 RUN curl -sL https://kylin.incubator.apache.org/download/kylin-0.7.2-incubating.tar.gz | tar -xz -C /usr/local/
 RUN cd /usr/local && ln -s ./kylin-0.7.2-incubating kylin
-RUN cd /usr/local/kylin && mvn clean install -DskipTests
 
 ADD HDP.repo /etc/yum.repos.d/
 RUN yum install -y pig hbase tez hadoop snappy snappy-devel hadoop-libhdfs ambari-log4j hive hive-hcatalog hive-webhcat webhcat-tar-hive webhcat-tar-pig mysql-connector-java mysql-server
-
-RUN curl -s http://xenia.sote.hu/ftp/mirrors/www.apache.org/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz | tar -xz -C /usr/local/
-ENV CATALINA_HOME /usr/local/apache-tomcat-7.0.59
-
-RUN curl -s http://nodejs.org/dist/v0.10.32/node-v0.10.32-linux-x64.tar.gz | tar -xz -C /usr/local/
-RUN ln -s /usr/local/node-v0.10.32-linux-x64/bin/npm /usr/bin/npm
-RUN ln -s /usr/local/node-v0.10.32-linux-x64/bin/node /usr/bin/node
 
 ADD serf /usr/local/serf
 
--- deploy.sh
+++ deploy.sh
@@ -17,69 +17,11 @@ echo ""
 echo "Welcome to use Kylin-Deploy script"
 echo "This script will help you:"
 echo "1. Check environment"
-echo "2. Build Kylin artifacts"
-echo "3. Prepare test cube related data"
-echo "4. Lauch a web service to build cube and query with (at http://localhost:7070/kylin)"
+echo "2. Prepare sample cube related data"
+echo "3. Lauch a web service to build cube and query with (at http://localhost:7070/kylin)"
 echo "Please make sure you're running this script on a hadoop CLI machine, and you have enough permissions."
-echo "Also, We assume you have installed: JAVA, TOMCAT, NPM and MAVEN."
-echo "[Warning] The installation may break existing tomcat applications on this CLI"
 echo ""
 
-echo "Checking JAVA status..."
-
-if [ -z "$JAVA_HOME" ]
-then
-    echo "Please set $JAVA_HOME so that Kylin-Deploy can proceed"
-    exit 1
-else
-    echo "JAVA_HOME is set to $JAVA_HOME"
-fi
-
-if [ -d "$JAVA_HOME" ]
-then
-    echo "$JAVA_HOME exists"
-else
-    echo " $JAVA_HOME does not exist or is not a directory."
-    exit 1
-fi
-
-echo "Checking tomcat status..."
-
-if [ -z "$CATALINA_HOME" ]
-then
-    echo "Please set CATALINA_HOME so that Kylin-Deploy knows where to start tomcat"
-    exit 1
-else
-    echo "CATALINA_HOME is set to $CATALINA_HOME"
-fi
-
-if [ -d "$CATALINA_HOME" ]
-then
-    echo "$CATALINA_HOME exists"
-else
-    echo " $CATALINA_HOME does not exist or is not a directory."
-    exit 1
-fi
-
-echo "Checking maven..."
-
-if [ -z "$(command -v mvn)" ]
-then
-    echo "Please install maven first so that Kylin-Deploy can proceed"
-    exit 1
-else
-    echo "maven check passed"
-fi
-
-echo "Checking npm..."
-
-if [ -z "$(command -v npm)" ]
-then
-    echo "Please install npm first so that Kylin-Deploy can proceed"
-    exit 1
-else
-    echo "npm check passed"
-fi
 
 KYLIN_HOME=/usr/local/kylin
 echo "Kylin home folder path is $KYLIN_HOME"
